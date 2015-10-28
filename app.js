0600bcea283d8b1ff7e2f38cf12283266523ce1c
angelorub94 <angelo.rubino94@gmail.com>
2015-10-28 17:58:16 +0100

    Stampa le 10 immagini + funzione click dello Start Button

--- app.js
+++ app.js
@@ -5,9 +5,24 @@
 // done: when the user has discovered the pair of cards
 // hidden: when the card has not been discovered yet
 
-$(document).ready(function(){
+var templ = '<li id="val-1" class="done"> <h3>val</h3></li>';
 
+/* <li id="0" class="hidden">
+          <h3>1</h3>         
+    </li> */
+var a;
+$(document).ready(function(){
+   for(var i=0; i<10; i++){
+       a = templ.replace("val-1", data[i]);
+       a = a.replace("val",data[i]);
+       $(".cards").append(a);
+   }  
   
+    $(".opt-start").click(function(){
+        $(".done").addClass("hidden");
+        $(".hidden").removeClass("done"); 
+    });
+
 });
 
 
